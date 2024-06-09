ec09d63bae94fe37dbdead2b6b70c66c8112ae10
RexWithLuv <aaronaax@gmail.com>
2024-06-10 00:54:03 +0200

    En el archivo login.php hemos pasado de mysqli a PDO

--- html/login.php
+++ html/login.php
@@ -10,48 +10,53 @@
 <body>
     <?php
     // Datos para la conexión a la BD
-    $servername = "172.20.0.2";
+    $host = "172.20.0.2";
+    $dbname = "albertoBD";
     $serveruser = "admin";
     $serverpasswd = "abc123.";
-    $database = "albertoBD";
+    $charset = "utf8mb4";
 
-    // Conexión en sí a la DB
-    $conn = new mysqli($servername, $serveruser, $serverpasswd, $database);
+    // Sentencia para conectarnos a la BD
+    $dsn = "mysql:host=$host;$dbname=$dbname;charset=$charset";
+
+    // Opciones para la BD
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
 
     // Si la conexión a la DB falla
-    if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
-    }
+    try {
+        // La línea para nos conecta a la BD
+        $pdo = new PDO($dsn, $serveruser, $serverpasswd, $options);
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") { // Preparamos la sentencia
-        // Sentencia SQL
-        $sql = "SELECT nombreCompleto, TRUNCATE(datediff(curdate(), fechaNac)/365, 0) AS edad, passwd FROM usuarios WHERE username = ?";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Sentencia SQL
+            $sql = "SELECT nombreCompleto, TRUNCATE(datediff(curdate(), fechaNacimiento)/365, 0) AS edad, password FROM usuarios WHERE username = ?";
 
-        // Preparamos
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $_POST["username"]);
+            // Preparamos
+            $stmt = $pdo->prepare($sql);
 
-        // Ejecutamos la consulta y almacenamos el resultado en una variable
-        $stmt->execute();
-        $resultado = $stmt->get_result();
+            // Ejecutamos
+            $stmt->execute([$_POST["username"]]);
 
-        // Usuario almacenará el resultado en un array
-        $usuario = $resultado->fetch_assoc();
+            // $usuario almacenará el resultado en un array
+            $usuario = $stmt->fetch();
 
-        // Comprobamos si la contraseña es correcta
-        if ($usuario["password"] === $_POST["password"] and count($usuario) > 0) {
-            echo "<h1>¡Bienvenido " . $usuario["nombreCompleto"] . "!</h1>";
-            echo "<h3>Ahora mismo tienes " . $usuario["edad"]  . " años :)</h3>";
+            // Comprobamos si la contraseña es correcta
+            if ($usuario["password"] === $_POST["password"] and count($usuario) > 0) {
+                echo "<h1>¡Bienvenido " . $usuario["nombreCompleto"] . "!</h1>";
+                echo "<h3>Ahora mismo tienes " . $usuario["edad"]  . " años :)</h3>";
+            } else {
+                echo "<h1>No he podido verificar tus datos, lo siento :(</h1>";
+            }
         } else {
-            echo "<h1>No he podido verificar tus datos, lo siento :(</h1>";
+            echo "<h1>No deberías estar aquí sin pasar por el formulario</h1>";
         }
-    } else {
-        echo "<h1>No deberías estar aquí sin pasar por el formulario</h1>";
-    }
-
-    // Cerramos la conexión
-    $conn->close();
-    ?>
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    } ?>
 </body>
 
 </html>
